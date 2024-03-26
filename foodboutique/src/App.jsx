86e1417cd417756ebb7279cf0813d03055f688f9
Anton Pedan <77937504+snap515@users.noreply.github.com>
2024-03-26 18:03:04 +0100

    adds header and footer

---
+++ foodboutique/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Header, Footer } from "./components/index"
+
+function App() {
+  return <>
+    <div className="container">
+      <Header />
+      <Footer/>
+    </div>
+  </>;
+}
+
+export default App;
---
+++ foodboutique/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+export const Header = () => {
+  return (
+    <div>
+      <div>
+        <a href="/">Food Boutique</a>
+        <nav>
+          <a href="/">Home</a>
+          <a href="/">Cart</a>
+        </nav>
+      </div>
+    </div>
+  )
+}
